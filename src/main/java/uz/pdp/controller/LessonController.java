0a1b4a79b8a1c0aa1a36c4f6298d3f4f5eb64910
abdisattarova <abdisattarovasevinch5@gmail.com>
2022-02-15 22:27:33 +0500

    Module Controller added

--- src/main/java/uz/pdp/controller/LessonController.java
+++ src/main/java/uz/pdp/controller/LessonController.java
@@ -3,20 +3,39 @@ package uz.pdp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import uz.pdp.dao.ModuleDao;
+import uz.pdp.dto.CourseDto;
 import uz.pdp.model.Lesson;
+import uz.pdp.model.Module;
+import uz.pdp.model.User;
 import uz.pdp.service.LessonService;
+import uz.pdp.service.ModuleService;
+
+import java.util.List;
 
 @Controller
 public class LessonController {
     @Autowired
     LessonService lessonService;
 
-    @RequestMapping(path = "/lessons/{id}")
-    public Lesson getLessonById(@PathVariable int id){
+    @Autowired
+    ModuleService moduleService;
 
-       return lessonService.getLessonById(id);
+    @RequestMapping(path = "/lessons/{id}")
+    public Lesson getLessonById(@PathVariable int id) {
+        return lessonService.getLessonById(id);
+
+    }
+
+    @RequestMapping(path = "lessons/form")
+    public String getLessonForm(Model model, @RequestParam(name = "id", required = false, defaultValue = "0") int id) {
+        return "";
     }
 }
+
+
